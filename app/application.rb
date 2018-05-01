4dc398e557fa44bdfe2a2795576b2d74e9ed9f1e
Michelle Olivieri <michelleo72@gmail.com>
2018-05-01 21:48:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-
+    if Time.now.hour < 12
+      resp.write "Good Morning!"
+    else
+      resp.wite "Good Afternoon!"
+    end
+    
   end
 
 end
